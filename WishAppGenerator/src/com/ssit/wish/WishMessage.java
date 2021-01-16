5eb83d29a82381068c37a76e39dbce6be160ca35
DELL <Siva@507>
2021-01-16 14:11:59 +0530

    dev1 added sub method

--- WishAppGenerator/src/com/ssit/wish/WishMessage.java
+++ WishAppGenerator/src/com/ssit/wish/WishMessage.java
@@ -3,16 +3,21 @@ package com.ssit.wish;
 import java.util.Date;
 
 public class WishMessage {
-public Date getDate()
-{
-	return new Date();
-}
-public static int sum(int x,int y)
-{
-	return x+y;
-}
-public static int sub(int x,int y)
-{
-	return x-y;
-}
+	public Date getDate() {
+		return new Date();
+	}
+
+	public static int sum(int x, int y) {
+		return x + y;
+	}
+
+	public static int sub(int x, int y) {
+		return x - y;
+	}
+
+	public static int mul(int x, int y)
+
+	{
+		return x * y;
+	}
 }

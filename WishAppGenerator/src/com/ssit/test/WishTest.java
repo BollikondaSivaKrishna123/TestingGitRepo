f2f52ef256858a56243c52725d42bee2ec9e9939
DELL <Siva@507>
2021-01-16 13:37:29 +0530

    initial proj by TL

---
+++ WishAppGenerator/src/com/ssit/test/WishTest.java
@@ -0,0 +1,16 @@
+package com.ssit.test;
+
+import com.ssit.wish.WishMessage;
+
+public class WishTest {
+
+	public static void main(String[] args) {
+		WishMessage wm=new WishMessage();
+		System.out.println(wm.getDate());
+ 
+		
+		
+		
+	}
+
+}
---
+++ WishAppGenerator/src/com/ssit/wish/WishMessage.java
@@ -0,0 +1,10 @@
+package com.ssit.wish;
+
+import java.util.Date;
+
+public class WishMessage {
+public Date getDate()
+{
+	return new Date();
+}
+}

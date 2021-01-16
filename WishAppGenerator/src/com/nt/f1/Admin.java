44bab9d51a21cb4f8eed95f3a98a0f41b2febbf7
DELL <Siva@507>
2021-01-16 14:37:46 +0530

    admin module-B1-dev

---
+++ WishAppGenerator/src/com/nt/f1/Admin.java
@@ -0,0 +1,8 @@
+package com.nt.f1;
+
+public class Admin {
+public void m1()
+{
+	System.out.println("Admin Module is Completed...");
+}
+}
---
+++ WishAppGenerator/src/com/nt/f1/Function1_Test.java
@@ -0,0 +1,8 @@
+package com.nt.f1;
+
+public class Function1_Test {
+public static void main(String[] args) {
+	Admin m=new Admin();
+	m.m1();
+}
+}
